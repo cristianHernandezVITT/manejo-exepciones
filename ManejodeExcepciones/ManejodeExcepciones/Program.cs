47231b1f9199abe0916a3f0efbfaa1ef7445b2a0
cristianHernandezVITT <105743030+cristianHernandezVITT@users.noreply.github.com>
2022-05-24 11:21:30 -0700

    Add files via upload

---
+++ ManejodeExcepciones/ManejodeExcepciones/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejodeExcepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables 
+            int x = 0;
+            double resultado = 0.0;
+            //Excepción
+            try
+            {
+                Console.Write("Indica un valor ");
+                x = int.Parse(Console.ReadLine());
+                resultado = Math.Pow(x, 2);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //Imprimir
+            if(x!=0)
+            {
+                Console.WriteLine("El resultado de {0} elevado al cuadrado es {1}", x, resultado);
+                Console.WriteLine("Presiona ENTER para salir");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Presiona ENTER para salir");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
